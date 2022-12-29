68b3ae5ce01067ed7028a4637a70bda7db6a5b63
tnals745 <tnals745@naver.com>
2022-12-29 13:12:49 +0900

    create bookInfo

---
+++ book_pet/lib/bookList.dart
@@ -0,0 +1,10 @@
+// final bookList = {
+//   "list": [
+//     {"image": "assets/images/img_blue.png", "name": "당신, 거기 있어줄래요?"},
+//     {"image": "assets/images/img_mint.png", "name": "센 강의 이름 모를 여인"},
+//     {"image": "assets/images/img_skyblue.png", "name": "안젤리크"},
+//     {"image": "assets/images/img_white.png", "name": "크리스마스 인터네셔널"}
+//   ]
+// };
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
