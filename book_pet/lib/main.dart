ca842f6e8f81505a741e98d88bbcbe146da98a1a
LJY0202 <113092549+LJY0202@users.noreply.github.com>
2022-12-29 10:41:26 +0900

    profile

--- book_pet/lib/main.dart
+++ book_pet/lib/main.dart
@@ -1,4 +1,36 @@
+import 'package:animated_theme_switcher/animated_theme_switcher.dart';
 import 'package:flutter/material.dart';
-import 'runApp.dart';
+import 'package:flutter/services.dart';
+import 'profilePage.dart';
+import 'themes.dart';
+import 'package:book_pet/utils/user_preferences.dart';
 
-void main() => runApp(const RunApp());
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  static final String title = 'User Profile';
+
+  @override
+  Widget build(BuildContext context) {
+    final user = UserPreferences.myUser;
+
+    return ThemeProvider(
+      initTheme: user.isDarkMode ? MyThemes.darkTheme : MyThemes.lightTheme,
+      child: Builder(
+        builder: (context) => MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: title,
+          home: ProfilePage(),
+        ),
+      ),
+    );
+  }
+}
